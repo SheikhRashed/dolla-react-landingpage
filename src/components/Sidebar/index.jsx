46ad2968e503ec868677a23923119c12c780ff54
SheikhRashed <devrashed06@gmail.com>
2021-09-05 00:27:58 +0600

    sidebar created

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarWrapper,
+  SideBtnWrap,
+} from './SidebarElements';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='home'>Home</SidebarLink>
+          <SidebarLink to='discover'>Discover</SidebarLink>
+          <SidebarLink to='service'>Services</SidebarLink>
+          <SidebarLink to='signup'>Sign up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
