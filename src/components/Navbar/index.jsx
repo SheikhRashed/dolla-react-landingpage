7d8b8f9ec73d3b501d752751c158beabe4e04124
SheikhRashed <devrashed06@gmail.com>
2021-09-04 22:07:13 +0600

    navbar created

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavBtn,
+  NavBtnLink,
+  NavItem,
+  NavLink,
+  NavLogo,
+  NavMenu,
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLink to='about'>About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='discover'>Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/services'>Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='signup'>Sign Up</NavLink>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
