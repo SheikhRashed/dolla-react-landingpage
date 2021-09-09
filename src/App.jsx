d33641a07e8c44da832e4864fc11043311ba8427
SheikhRashed <devrashed06@gmail.com>
2021-09-09 23:38:17 +0600

    signin page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages';
+import signinPage from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={signinPage} exact />
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/signin.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+const signinPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <>
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+    </>
+  );
+};
+
+export default signinPage;
